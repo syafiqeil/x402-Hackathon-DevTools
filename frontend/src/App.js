2f4e05b0663edf78144c8b8b07d1bdd04826c448
syafiqeil <syafnabil12@gmail.com>
2025-11-06 17:14:48 +0700

    Menambahkan file index.html dan index.js untuk frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,9 +8,7 @@ import {
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import { PhantomWalletAdapter } from "@solana/wallet-adapter-phantom";
 import { clusterApiUrl } from "@solana/web3.js";
-
-require("@solana/wallet-adapter-react-ui/styles.css");
-
+import "@solana/wallet-adapter-react-ui/styles.css";
 import PremiumContent from "./PremiumContent"; 
 
 function App() {
